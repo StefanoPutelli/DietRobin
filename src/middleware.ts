0da9725c4afe55a4644a1a28f62dfb85289360d0
StefanoPutelli <putellistefano@gmail.com>
2025-04-16 18:05:14 +0200

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,21 +15,21 @@ export default async function middleware(request: NextRequest) {
       return NextResponse.next();
     }
   }
-  if(url.pathname.startsWith('/client')) {
-    const session = await authOptionsClient.auth();
-    if (!session || session.user.role !== 'client') 
-      return NextResponse.redirect(new URL('/login', request.url))
-  } else if(url.pathname.startsWith('/nutritionist')) {
-    const session = await authOptionsNutritionist.auth();
-    if (!session || session.user.role !== 'nutritionist') {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  } else if(url.pathname.startsWith('/api/nutritionist')) {
-    const session = await authOptionsNutritionist.auth();
-    if(!session || session.user.role !== 'nutritionist') {
-      // return access denied
-      return NextResponse.json({ error: 'Access Denied' }, { status: 403 })
-    }
-  }
+  // if(url.pathname.startsWith('/client')) {
+  //   const session = await authOptionsClient.auth();
+  //   if (!session || session.user.role !== 'client') 
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  // } else if(url.pathname.startsWith('/nutritionist')) {
+  //   const session = await authOptionsNutritionist.auth();
+  //   if (!session || session.user.role !== 'nutritionist') {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
+  // } else if(url.pathname.startsWith('/api/nutritionist')) {
+  //   const session = await authOptionsNutritionist.auth();
+  //   if(!session || session.user.role !== 'nutritionist') {
+  //     // return access denied
+  //     return NextResponse.json({ error: 'Access Denied' }, { status: 403 })
+  //   }
+  // }
   return NextResponse.next()
 }
